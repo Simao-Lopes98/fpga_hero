9715fd595cf1736b9725f08ab1332fe58de30c5b
simao.lopes98 <simaolopes98@gmail.com>
2026-05-10 17:11:34 +0100

    and gate with a few tricks

--- 01_and_gate/and_gate.v
+++ 01_and_gate/and_gate.v
@@ -1,14 +1,23 @@
 /* 
-AND GATE - When buttons 1 and 2 are pressed, turn on LED
+AND GATE - Btn 0 turns on 2 LEDs, btn 0 and 1 turn on another LED
 */
 
 
 module and_gate (
-        input pmod_0,
-        input pmod_1,
-        output led_0
+        input   [1:0] pmod,
+        output  [2:0] led
         );
 
-assign led_0 = ~pmod_1 & ~pmod_0;
+    wire not_pmod_0;
+
+    assign not_pmod_0 = ~pmod[0];
+
+    /* same as:
+    assign led[0] = not_pmod_0;
+    assign led[1] = not_pmod_0;
+    */
+    assign led[1:0] = {2{not_pmod_0}};
+
+    assign led[2] = not_pmod_0 & ~pmod[1];
 
 endmodule
